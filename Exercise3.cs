9983c6452fa41538e92584c2ec7a95c2e82bae5e
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-07 02:44:41 +0300

    Incrementing numbers in a list by using delegates, extention methods, and LINQ

--- Exercise3.cs
+++ Exercise3.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using LINQExercise3;
 namespace LINQExercise3
 {
     class Exercise3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<int> ListOfRandomNumbers = new List<int>{ 1, 2, 3, 4, 5 };
+            IEnumerable<int> ListOfNumbersAfterTransformation = ListOfRandomNumbers.Transform(IncrementInt);
+
+            foreach(var number in ListOfNumbersAfterTransformation)
+            {
+                Console.WriteLine(number);
+            }
         }
+
+        private static int IncrementInt(int IntNumber)
+        {
+            return IntNumber + 1;
+        }
+
+
+
     }
 }
---
+++ ExtensionForIEnumerableOfInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQExercise3
+{
+    public static class  ExtensionForIEnumerableOfInt
+    {
+        public delegate int TransformationFunction(int num);
+        public static IEnumerable<int> Transform(this IEnumerable<int> num, TransformationFunction func)
+    {
+
+        foreach (var value in num)
+        {
+            yield return func(value);
+        }
+
+    }
+}
+}
+
