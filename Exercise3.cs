021657d9aa46cf20a8ae2f7e24e14760d57dca7f
Lujain Abusada <Lujain.Abusada@gmail.com>
2021-07-07 02:22:15 +0300

    Exercise 3 on extention methods and delagates

---
+++ Exercise3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQExercise3
+{
+    class Exercise3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
